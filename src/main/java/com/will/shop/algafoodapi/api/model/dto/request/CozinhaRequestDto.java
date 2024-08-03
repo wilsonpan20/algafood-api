d2eaef4ad28a26a2d06bee9f5897472a37365cc6
Wilson Evangelista <wilson.evangelista@icarotech.com>
2024-08-03 09:34:28 -0300

    fetaure: add dto cozinha and restaurantes

--- src/main/java/com/will/shop/algafoodapi/api/model/dto/request/CozinhaRequestDto.java
+++ src/main/java/com/will/shop/algafoodapi/api/model/dto/request/CozinhaRequestDto.java
@@ -1,11 +1,12 @@
-package com.will.shop.algafoodapi.api.model;
+package com.will.shop.algafoodapi.api.model.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CozinhaRequestDto {
+	@NotNull
 	private Long id;
-	private String nome;
 }
